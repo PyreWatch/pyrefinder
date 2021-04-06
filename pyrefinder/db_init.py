cee7597d2b9a617a64970acedd129f553617d8ff
Paolo Garcia <paologarcia002@outlook.com>
2021-04-05 20:50:11 -0400

    inital trigger development

--- pyrefinder/db_init.py
+++ pyrefinder/db_init.py
@@ -9,15 +9,21 @@ create table fighter (
   id text primary key,
   latitude decimal not null,
   longitude decimal not null,
-  last_status text
+  last_status text,
+  last_image_path text
 );
 drop table if exists fighter_data ;
 create table fighter_data (
   client_id text,
   status text not null,
   time text not null,
+  image_path text,
   foreign key(client_id) references fighter(id)
 );
+create trigger update_image after insert on fighter_data
+begin
+  update fighter set last_image_path = new.image_path where fighter.id = new.client_id 
+end;
 """
 
 conn = sqlite3.connect(DB_Name)
